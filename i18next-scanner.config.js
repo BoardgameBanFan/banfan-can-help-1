20cbd945016631ce90b3a5e9f1d385acae29f43b
GoreStarry <sunrise91.t3@gmail.com>
2025-02-25 10:07:29 +0800

    [feat] add i18n support

---
+++ i18next-scanner.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+module.exports = {
+  input: ['app/**/*.{js,ts,jsx,tsx}', 'components/**/*.{js,ts,jsx,tsx}'],
+  output: './',
+  options: {
+    defaultLng: 'de',
+    lngs: ['zh-TW', 'en', 'de'],
+    ns: ['common'],
+    defaultNs: 'common',
+    resource: {
+      loadPath: 'public/locales/{{lng}}/{{ns}}.json',
+      savePath: 'public/locales/{{lng}}/{{ns}}.json',
+    },
+    func: {
+      list: ['t'],
+      extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    },
+  },
+};
