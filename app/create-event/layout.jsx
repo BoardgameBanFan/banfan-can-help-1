0bfbb7ffa2e5eea2c5f1f2ac3009fc2f6d90972f
Lance Huang <lance.gs.huang@viewsonic.com>
2025-03-05 16:59:32 +0800

    fix: remove duplicate title in create-event layout

--- app/create-event/layout.jsx
+++ app/create-event/layout.jsx
@@ -1,8 +1,3 @@
 export default function CreateEventLayout({ children }) {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">創建活動</h1>
-      {children}
-    </div>
-  );
+  return <div className="p-4">{children}</div>;
 }
