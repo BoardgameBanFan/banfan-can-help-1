fd60271cb2b99d21a25dbc1ad30886be1e476659
LanceHuang <zxc321zxc0@gmail.com>
2025-03-21 23:15:52 +0800

    remove /create layout padding

--- app/event/create/layout.jsx
+++ app/event/create/layout.jsx
@@ -3,7 +3,7 @@ import { SWRProvider } from "@/lib/swr-config.jsx";
 export default function CreateEventLayout({ children }) {
   return (
     <SWRProvider>
-      <div className="p-4">{children}</div>
+      <div className="">{children}</div>
     </SWRProvider>
   );
 }
